ca53cee4e2c3aed3bdcf78e279010b47b358b95e
kykakre <kykakre@mail.ru>
2022-10-20 18:07:36 +0400

    layout

---
+++ src/models/taskModel.ts
@@ -0,0 +1,17 @@
+export interface ITasks {
+    child: any[];
+    equipmentCosts: number;
+    estimatedProfit: number;
+    id: number;
+    machineOperatorSalary: number;
+    mainCosts: number;
+    materials: number;
+    mimExploitation: number;
+    overheads: number;
+    rowName: string;
+    salary: number;
+    supportCosts: number;
+    total: number;
+}
+
+
